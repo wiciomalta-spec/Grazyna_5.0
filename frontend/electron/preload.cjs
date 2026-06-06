ac530cea3bdcb505074415a58bf18e2e77c48577
wiciomalta-spec <wiciomalta@wp.pl>
2026-06-06 06:50:58 +0200

    Dodaj preload i foldery wersji

---
+++ frontend/electron/preload.cjs
@@ -0,0 +1,14 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
+  listSerialPorts: async () => {
+    try {
+      const { SerialPort } = require('serialport');
+      return await SerialPort.list();
+    } catch (error) {
+      console.error("SerialPort not available:", error);
+      return [];
+    }
+  }
+});
