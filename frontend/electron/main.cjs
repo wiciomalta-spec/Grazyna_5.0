2a54c7924f41901b2b8c369e19753f8b8b7bb4e6
wiciomalta-spec <wiciomalta@wp.pl>
2026-06-06 06:50:41 +0200

    Poprawki frontend: electron, package.json, tsconfig

--- frontend/electron/main.cjs
+++ frontend/electron/main.cjs
@@ -7,27 +7,30 @@ function createWindow() {
     height: 800,
     minWidth: 1024,
     minHeight: 700,
+    title: 'GRAŻYNA 5.0',
+    icon: path.join(__dirname, '../assets/icon.ico'),
     webPreferences: {
       contextIsolation: true,
-      nodeIntegration: false
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'preload.cjs')
     }
   });
 
-  win.loadFile(path.join(__dirname, '../dist/index.html'));
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
-
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
